d5b890a55e9d6d32793071060284da7ddc501491
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-08-05 19:34:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,20 @@ function preload(){
 function setup(){
     canvas=createCanvas(400,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(400,400);
+    video.position(80,100);
+    posenet=ml5.poseNet(video,modelloaded);
+    posenet.on('pose',gotposes);
+}
+function modelloaded(){
+    console.log("posenet is loaded");
 }
 function draw(){
+    background("lightgreen");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+    }
 }
